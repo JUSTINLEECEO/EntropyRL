1a2249c7638dd066aee3ee5206b8d22af871c7f9
JustinLee <liyilin784@126.com>
2025-08-23 15:59:21 +0800

    Dataset changed.

--- examples/qwen2_5_vl_3b_geo3k_grpo.sh
+++ examples/qwen2_5_vl_3b_geo3k_grpo.sh
@@ -8,9 +8,10 @@ MODEL_PATH=/share/liyilin-nfs/models/Qwen2.5-VL-3B-Instruct
 
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
-    data.train_files=hiyouga/geometry3k@train \
-    data.val_files=hiyouga/geometry3k@test \
+    data.train_files=JustinLeeCEO/geometry3k@train \
+    data.val_files=JustinLeeCEO/geometry3k@test \
     worker.actor.model.model_path=${MODEL_PATH} \
     worker.rollout.tensor_parallel_size=1 \
     trainer.experiment_name=qwen2_5_vl_3b_geo_grpo \
-    trainer.n_gpus_per_node=8
+    trainer.n_gpus_per_node=8 \
+    trainer.save_debug_path=null
