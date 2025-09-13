8990aad1d321c4c42320eb044dbf22c3a3de34dc
JustinLee <liyilin784@126.com>
2025-09-13 22:54:40 +0800

    Scripts added.

--- examples/qwen2_5_vl_3b_mscoco_grpo.sh
+++ examples/qwen2_5_vl_3b_mscoco_grpo.sh
@@ -14,5 +14,5 @@ python3 -m verl.trainer.main \
     worker.rollout.tensor_parallel_size=1 \
     trainer.experiment_name=qwen2_5_vl_3b_geo_grpo \
     trainer.n_gpus_per_node=8 \
-    trainer.save_debug_path=./debug \
+    trainer.save_debug_path=null \
     worker.reward.reward_function=./examples/reward_function/coco_cider.py:compute_score
---
+++ examples/qwen2_5_vl_7b_mscoco_grpo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+export PYTHONUNBUFFERED=1
+
+MODEL_PATH=/share/liyilin-nfs/models/Qwen2.5-VL-7B-Instruct
+
+python3 -m verl.trainer.main \
+    config=examples/config.yaml \
+    data.train_files=JustinLeeCEO/MSCOCO2014@train \
+    data.val_files=JustinLeeCEO/MSCOCO2014@test \
+    worker.actor.model.model_path=${MODEL_PATH} \
+    worker.rollout.tensor_parallel_size=1 \
+    trainer.experiment_name=qwen2_5_vl_7b_geo_grpo \
+    trainer.n_gpus_per_node=8 \
+    trainer.save_debug_path=null \
+    worker.reward.reward_function=./examples/reward_function/coco_cider.py:compute_score
